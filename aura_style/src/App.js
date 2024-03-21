bf58e84068cc76161bc8f175b9d075460582e8f4
shashankpatil399 <shashankp[atil399.com>
2024-03-21 18:49:33 +0530

    implement forget adn reset passord

--- aura_style/src/App.js
+++ aura_style/src/App.js
@@ -1,70 +1,3 @@
-// import './App.css';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// import Signup from './components/Signup';
-// import Login from "./components/Login";
-// import Forget from './components/forget';
-// import Reset from "./components/reset"
-// import Dashboard from './components/Dashboard';
-
-// function App() {
-//   return ( 
-    
-//     <div >
-//     <BrowserRouter>
-//       <Routes>
-//       <Route exact path="/" element={<Signup />} />
-//       <Route exact path="/Login" element={<Login />} />
-//       <Route exact path="/Dashboard" element={<Dashboard />} />
-//       <Route exact path="/forget" element={<Forget />} />
-//       <Route exact path="/Reset" element={<Reset />} />
-        
-       
-//       </Routes>
-//     </BrowserRouter>
-//     </div>
-//   );
-// }
-// export default App;
-
-
-
-// import React from 'react';
-// import { useRoutes } from 'react-router-dom';
-// import Signup from './components/Signup';
-// import Login from "./components/Login";
-// import Forget from './components/forget';
-// import Reset from "./components/reset"
-// import Dashboard from './components/Dashboard';
-// import ProtectedRoute from './components/protectedRoute';
-// // import { Provider } from 'react-redux';
-// const routes = [
-//   {
-//     path: '/',
-//     element: <ProtectedRoute element={<Dashboard/>}/>,
-//     children: [
-//       { path: 'Dashboard', element: <Dashboard /> }
-//     ]
-//   },
-//   { path: '/Signup', element: <Signup /> },
-//   { path: '/Login',  element: <Login /> },
-//   { path: '/Forget', element: <Forget /> },
-//   { path: '/Reset',  element: <Reset /> }
-  
-
-// ];
-
-// const App = () => {
-//   const routing = useRoutes(routes);
-
-//   return routing;
-// };
-
-// export default App;
-
-
-
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './components/Signup';
@@ -72,15 +5,19 @@ import Login from "./components/Login";
 import Forget from './components/forget';
 import Reset from "./components/reset"
 import Dashboard from './components/Dashboard';
+import Header from './components/Header';
+import Sidebarapp from './components/Sidebar';
 import ProtectedRoute from './components/protectedRoute';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ProtectedRoute element={<Dashboard/>} />}>
+        <Route path="/" element={<ProtectedRoute element={<Dashboard />} />}>
           <Route path="Dashboard" element={<Dashboard />} />
         </Route>
+        <Route path="Header" element={<Header />} />
+        <Route path="Sidebarapp" element={<Sidebarapp />} />
         <Route path="Signup" element={<Signup />} />
         <Route path="Login" element={<Login />} />
         <Route path="Forget" element={<Forget />} />
--- aura_style/src/components/Dashboard.js
+++ aura_style/src/components/Dashboard.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from './Header'
+import  Sidebar  from "./Sidebar"
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <Header/>
+<Sidebar/>
+
+    </div>
+)
 }
 
 export default Dashboard
